0e2750596d75ec84979fd53f3fbadd2bd993a56b
Adriana <adrimedinamontiel12@gmail.com>
2023-06-09 13:59:26 -0600

    04 operadores

---
+++ operadores.py
@@ -0,0 +1,18 @@
+#operadores matematicos
+#+-*%
+# / division float 
+# // division entera
+#**potencia
+print(2+2)
+print(2-2)
+print(2*2)
+print(2%2)
+print(10/3)
+print(10//3)
+print(3**3)
+
+#operadores para strings 
+#+(contatena)*(replica)
+
+print("hola"+"mundo")
+print("hola\n"*4)
