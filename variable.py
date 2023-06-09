1a469f98f7099b9dc28e526cca8c7ddf5c0dda72
Adriana <adrimedinamontiel12@gmail.com>
2023-06-09 13:24:09 -0600

    02 varianbles

---
+++ variable.py
@@ -0,0 +1,12 @@
+#Python es no Tipado
+
+name="Adiana M edina"
+age=39
+height=1.68
+is_student = False
+children=[]
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(children))
