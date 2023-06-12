f2d28476731f7641cce6ddf4d4ed9b01b1034304
Adriana <adrimedinamontiel12@gmail.com>
2023-06-12 09:53:12 -0600

    05 operadores logicos y booleanos

---
+++ operadores_2.py
@@ -0,0 +1,22 @@
+#operadores booleanos
+#>>-
+print(10>10)
+print(10>=10)
+#< <=
+print(10<10)
+print(10<=10)
+#==
+print(10==10)
+#!=
+print(10!=10)
+
+#operadores logicos
+#and (true if all true)
+print(True and True)
+print(True and False)
+#or (true if al least one true)
+print(True or True)
+print(True or False)
+#not (not True)
+print(not True)
+print(not False)
