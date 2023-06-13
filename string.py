76043c7a98a04df54e30cceafe3112c4e8dbc1be
Adriana <adrimedinamontiel12@gmail.com>
2023-06-13 14:16:43 -0600

    07 strings

---
+++ string.py
@@ -0,0 +1,29 @@
+my_string ="hola MuNdO"
+
+#mayusculas
+
+print(my_string.upper())
+
+#minusculas
+print(my_string.lower())
+
+
+#longitud de string
+print(len(my_string))
+
+#quitar espacios
+print(my_string.strip())
+
+my_string = "este es un nuevo texto"
+
+#indexing and sliciong
+print(my_string[0])
+print(my_string[3])
+print(my_string[6])
+
+print(my_string[-1])
+print(my_string[-2])
+
+print(my_string[0:5])
+print(my_string[2:5])
+print(my_string[5:])
